d5944cb4c54b0df28c0a9435525c040589a6e91e
Junichi Hashimoto <junichi_hashimoto@hotmail.co.jp>
2021-07-10 20:43:45 +0900

    created project

---
+++ JHashimoto.BacklogCli/JHashimoto.BacklogCli/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JHashimoto.BacklogCli
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
